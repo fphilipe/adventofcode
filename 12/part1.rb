3c1218e16ae094134e1779ac5a8419f9a61212a1
Philipe Fatio <me@phili.pe>
2021-12-15 16:17:13 +0100

    Day 12: Ruby

---
+++ 12/part1.rb
@@ -0,0 +1,23 @@
+String.class_exec { def small? = downcase == self }
+
+graph = ARGF.readlines
+  .map { _1.chomp.split('-') }
+  .map { [_1, _1.reverse] }
+  .flatten(1)
+  .group_by(&:first)
+  .transform_values { _1.map(&:last) }
+
+def find(node, graph, path=[])
+  return if node.small? && path.include?(node)
+
+  path += [node]
+  if node == "end"
+    return path.join(",")
+  end
+
+  graph[node]
+    .flat_map { |node| find(node, graph, path) }
+    .compact
+end
+
+puts find("start", graph).length
